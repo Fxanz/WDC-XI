f1baee3ff32e75b97d0aa51ac61d13f00c9480fc
Fxanz <107518652+Fxanz@users.noreply.github.com>
2022-09-13 06:43:23 +0800

    WDC

---
+++ admin/nilai.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<link rel="stylesheet" href="../css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css.map">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="../fontawesome-free/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous" />
+  </head>
+  <nav class="navbar navbar-expand-lg bg-dark fixed-top">
+      <div class="container-fluid">
+        <a class="navbar-brand text-white" style="margin-left:50px" href="#">Data Siswa SMK TI BALI GLOBAL DENPASAR</a>
+        <div class="pojok-kanan">
+          <form class="d-flex" role="search" style="margin-left:500px;">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <button class="btn btn-outline-success" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+          </form>
+          </div>
+      </div>
+    </nav>
+    <div class="row gx-0 gy-0 mt-5">
+      <div class="col-md-2 bg-dark mt-2 px-3 py-4">
+        <ul class="nav flex-column mx-3 mb-5">
+
+
+          <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle dropdown-utama" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+        <i class="fa-solid fa-user-graduate mx-2"></i>  Kelas
+        </a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="#">X RPL 1</a>
+          <a class="dropdown-item" href="#">X RPL 2</a>
+          <a class="dropdown-item" href="#">X RPL 3</a>
+        </div>
+
+      </li> <hr class="hr-2">
+      <li class="nav-item">
+            <a class="nav-link active text-white" aria-current="page" href="#"><i class="fa-solid fa-house mx-2"></i> Menu Utama</a>
+          </li>
+          <hr class="hr-2">
+          <li class="nav-item">
+            <a class="nav-link text-white" href="#"><i class="fa-solid fa-chalkboard-user mx-2"></i> Daftar guru</a>
+          </li>
+          <hr class="hr-1">
+          <li class="nav-item">
+            <a class="nav-link text-white" href="#"><i class="fa-solid fa-calendar mx-2"></i> Jadwal Ujian</a>
+          </li>
+          <hr class="hr-1">
+          <li class="nav-item">
+            <a class="nav-link text-white" href="#"><i class="fa-solid fa-marker mx-2 "></i> Nilai Ujian</a>
+          </li>
+          <hr class="hr-1">
+        <li class="nav-item">
+            <a href="../logout.php" class="nav-link active text-white" aria-current="page" href="#"><i class="fa-solid fa-power-off mx-2"></i>logout</a>
+          </li>
+
+
+        </ul>
+
+        <script src="../js/bootstrap.bundle.min.js"></script>
+        <script src="../https://cdn.jsdelivr.net/npm/chart.js@3.0.2/dist/chart.min.js"></script>
+        <script src="../js/jquery-3.5.1.js"></script>
+        <script src="../js/jquery.dataTables.min.js"></script>
+        <script src="../js/dataTables.bootstrap5.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+        <script src="js/script.js"></script>
+</body>
+</html>
